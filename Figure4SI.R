6cc58abf719d2a259ff98f9e465b014cb1204c9e
Juan Luis Herrera Cortijo <juan.luis.herrera.cortijo@gmail.com>
2018-03-21 10:31:57 +0100

    Script finished

---
+++ Figure4SI.R
@@ -0,0 +1,89 @@
+#' Reads ecological_indicators.csv, institutional_indicators.csv and 
+#' socioeconomic_indicators.csv. Computes correlation matrices for each set of
+#' indicators and saves the result in Fig 4 Si.png
+
+##### 1. LOAD PACKAGES AND DISPLAY VERSIONS #####
+
+version                           
+# platform       x86_64-apple-darwin13.4.0   
+# arch           x86_64                      
+# os             darwin13.4.0                
+# system         x86_64, darwin13.4.0        
+# status                                     
+# major          3                           
+# minor          3.0                         
+# year           2016                        
+# month          05                          
+# day            03                          
+# svn rev        70573                       
+# language       R                           
+# version.string R version 3.3.0 (2016-05-03)
+# nickname       Supposedly Educational          
+
+if(!require(gridExtra)){
+  install.packages("gridExtra",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+require(gridExtra)
+packageVersion("gridExtra")
+# [1] ‘2.2.1’
+
+if(!require(corrplot)){
+  install.packages("corrplot",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+require(corrplot)
+packageVersion("corrplot")
+# [1] ‘0.77’
+
+if(!require(data.table)){
+  install.packages("data.table",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+require(data.table)
+packageVersion("data.table")
+# [1] ‘1.10.4’
+
+if(!require(tidyverse)){
+  install.packages("tidyverse",dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+require(tidyverse)
+packageVersion("tidyverse")
+# [1] ‘1.2.1’
+
+
+
+##### 2. READ DATA #####
+
+# Read the data
+
+eco_indicators <- fread("data/ecological_indicators.csv")
+ins_indicators <- fread("data/institutional_indicators.csv")
+soc_indicators <- fread("data/socioeconomic_indicators.csv")
+
+##### 3. PLOT #####
+
+png("Figures/Fig 4 SI.png",width=12.5,height = 4.5,units = "in",res=300)
+
+par(mfrow = c(1, 3))
+
+# socioeconomic correlations
+
+soc_to_correlate <- soc_indicators[, 4:10]
+soc_correlations <- cor(soc_to_correlate, use="pairwise.complete.obs")
+p1 <- corrplot(soc_correlations, order ="AOE")
+
+
+# institutional correlations
+
+ins_to_correlate <- ins_indicators[, 4:7]
+ins_correlations <- cor(ins_to_correlate, use="pairwise.complete.obs")
+p2 <- corrplot(ins_correlations, order ="AOE")
+
+
+# Ecological correlations
+
+eco_to_correlate <- eco_indicators[, 3:13]
+eco_correlations <- cor(eco_to_correlate, use="pairwise.complete.obs")
+p3 <- corrplot(eco_correlations, order ="AOE")
+
+par(mfrow=c(1,1))
+
+dev.off()
