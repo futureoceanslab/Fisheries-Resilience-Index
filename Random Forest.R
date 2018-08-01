96c2ed69d14abc5cd52894cb98e368fe39041e86
Elefontan <elena.fontan.alende@gmail.com>
2018-08-01 19:21:30 +0200

    random forest

--- Random Forest.R
+++ Random Forest.R
@@ -4,9 +4,16 @@
 if(!require(randomForest)){
   install.packages('randomForest',dependencies = TRUE,repos='http://cran.us.r-project.org')
 }
+if(!require(ggRandomForests)){
+  install.packages('ggRandomForests',dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+if(!require(MASS)){
+  install.packages('MASS',dependencies = TRUE,repos='http://cran.us.r-project.org')
+}
+library(MASS)
 library(randomForest)
-
-rf_classifier = randomForest(Species ~ ., data=training, ntree=100, mtry=2, importance=TRUE)
+library("ggRandomForests") 
+library(ggplot2)
 
 
 
@@ -14,22 +21,67 @@ rf_classifier = randomForest(Species ~ ., data=training, ntree=100, mtry=2, impo
 
 
 eco_factors <- fread("data/ecological_factors_country.csv") %>% mutate(DIMENSION="ecological")
+eco_factors<- eco_factors[,3:6]
 
 ins_factors <- fread("data/institutional_factors_country.csv")  %>% mutate(DIMENSION="institutional")
+ins_factors<- ins_factors[,3:6]
 
 soc_factors <- fread("data/socioeconomic_factors_country.csv")  %>% mutate(DIMENSION="socioeconomic")
+soc_factors<- soc_factors[,3:5]
 
-##### 2. Compute Resilience Index #####
-
-# Compute resilience index. For that put together all the factors computed above and compute 
-# their mean by species, dimension and country
-
-resilience_index <-cbind (eco_factors, soc_factors, by=COUNTRIES)
+resilience_index <- fread("data/final_index.csv") 
+resilience_index<- resilience_index[,c(9)]
 
 
-resilience_index <- bind_rows(eco_factors,ins_factors,soc_factors) %>% 
-  gather(FACTOR,VALUE,-COUNTRIES,-SPECIES,-DIMENSION,factor_key = TRUE) %>%
-  filter(complete.cases(.)) %>%
-  group_by(SPECIES,DIMENSION,COUNTRIES) %>% 
-  summarise(Resilience_Index=mean(VALUE,na.rm=TRUE)) %>%
-  ungroup()
+##### 2. Compute dataframe #####
+
+rf1 <- cbind(eco_factors, ins_factors)
+str(rf1)
+rf2 <- cbind(rf1, soc_factors)
+str(rf2)
+rf <- cbind(rf2, resilience_index)
+str(rf)
+
+##### 3. Random Forest #####
+dim(rf)
+#[1] 66 12
+
+train=sample(1:nrow(rf),20) #training Sample with 20 observations
+index.rf=randomForest(Resilience_Index ~ . , data = rf , subset = train, na.action=na.roughfix, calibrate = TRUE)
+index.rf
+plot(index.rf)
+
+
+plot(gg_vimp(index.rf))
+
+
+train=sample(1:nrow(rf),50) #training Sample with 50 observations
+index.rf=randomForest(Resilience_Index ~ . , data = rf , subset = train, na.action=na.roughfix, calibrate = TRUE)
+index.rf
+plot(index.rf)
+
+
+plot(gg_vimp(index.rf))
+
+
+#no training
+index.rf=randomForest(Resilience_Index ~ . , data = rf ,  na.action=na.roughfix)
+index.rf
+plot(index.rf)
+
+
+plot(gg_vimp(index.rf))
+
+
+#BOSTON data
+
+data("Boston")
+str(Boston)
+dim(Boston)
+trainB=sample(1:nrow(Boston),300) #training Sample with 50 observations
+a=randomForest(medv ~ . , data = Boston , subset = trainB, na.action=na.roughfix)
+a
+plot(a)
+
+
+plot(gg_vimp(a))
